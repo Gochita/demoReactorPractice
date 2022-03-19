847b708cd88a014f4fbdcdf8fe0b8398580c778a
tamagochita <mevere66@gmail.com>
2022-03-19 17:21:05 -0400

    Ejemplos flux, mono y flux mono combinados

--- src/main/java/com/example/demoreactor/DemoReactorApplication.java
+++ src/main/java/com/example/demoreactor/DemoReactorApplication.java
@@ -1,33 +1,73 @@
 package com.example.demoreactor;
+
 import io.reactivex.Observable;
 import model.Persona;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class DemoReactorApplication {
 
-	private static final Logger Log = LoggerFactory.getLogger(DemoReactorApplication.class);
+    private static final Logger Log = LoggerFactory.getLogger(DemoReactorApplication.class);
 
 
-	public static void reactor(){
-		Mono.just(new Persona(1,"Gochi",21))
-				.subscribe(p -> Log.info("[Reactor] Persona: "+ p));
-
-	}
-	public static void rxjava2(){
-		Observable.just(new Persona(1,"Gochi",21))
-				.subscribe(p -> Log.info("[Rxjava2] Persona: "+ p));
-	}
+    public static void reactor() {
+        Mono.just(new Persona(1, "Gochi", 21))
+                .doOnNext(p -> {
+                    Log.info("[Reactor] Persona: " + p);
+                })
+                .subscribe(p -> Log.info("[Reactor] Persona: " + p));
 
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoReactorApplication.class, args);
-		reactor();
-		rxjava2();
-	}
+    }
+
+    public static void rxjava2() {
+        Observable.just(new Persona(1, "Gochi", 21))
+                .doOnNext(p -> Log.info("[Rxjava2] Persona: " + p))
+                .subscribe(p -> Log.info("[Rxjava2] Persona: " + p));
+    }
+
+    public static void mono() {
+        Mono.just(new Persona(2, "mito", 22))
+                .subscribe(p -> Log.info(p.toString()));
+    }
+
+    public static void flux() {
+        //Se suscribe a un arreglo de personas, los imprime individualmente
+        List<Persona> personas = new ArrayList<>();
+        personas.add(new Persona(1, "vicci", 40));
+        personas.add(new Persona(2, "evelyn", 33));
+        personas.add(new Persona(3, "eleonora", 70));
+
+        Flux.fromIterable(personas)
+                .subscribe(p -> Log.info(p.toString()));
+
+
+    }
+    public static void fluxMono(){
+        //se suscribe a un arreglo de personas, los imprime en un solo bloque
+        List<Persona> personas = new ArrayList<>();
+        personas.add(new Persona(1, "vicci", 40));
+        personas.add(new Persona(2, "evelyn", 33));
+        personas.add(new Persona(3, "eleonora", 70));
+
+        Flux <Persona> fx = Flux.fromIterable(personas);
+        fx.collectList().subscribe(lista -> Log.info(lista.toString()));
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoReactorApplication.class, args);
+        fluxMono();
+        // mono();
+        //flux();
+    }
 
 }
